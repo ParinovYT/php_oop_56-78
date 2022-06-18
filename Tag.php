eca8c671ff15b9ca323c5e563a42297a52b18acc
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:13:52 +0300

    63

--- Tag.php
+++ Tag.php
@@ -41,7 +41,12 @@
 				$result = '';
 				
 				foreach ($attrs as $name => $value) {
-					$result .= " $name=\"$value\"";
+					// Если значение атрибута равно true:
+					if ($value === true) {
+						$result .= " $name"; // это атрибут без значения
+					} else {
+						$result .= " $name=\"$value\""; // это атрибут со значением
+					}
 				}
 				
 				return $result;
@@ -57,5 +62,7 @@
 			}	
 			return $this;
 		}
+		
+		
 	}
 ?>
--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 	$tag = new Tag('input');
 	
 	echo $tag
-		->setAttrs(['id' => 'test', 'class' => 'eee'])
-		->setAttr('type', 'text')
-		->open(); 
+		->setAttr('id', 'test')
+		->setAttr('disabled', true)
+		->open();
 		
-		// выведет <input id="test" class="eee" type="text">
+		// выведет <input id="test" disabled>
 	
 ?>
