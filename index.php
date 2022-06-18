a85468cf055738c6cb4ddc9090ba4702018ae1ca
Danila <gamer.xokks@yandex.ru>
2022-06-18 13:48:26 +0300

    75

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
 	require_once 'Input.php';
 	require_once 'Submit.php';
 	require_once 'Password.php';
-		
+	require_once 'Hidden.php';
+	
 	$form = (new Form)->setAttrs([
 		'action' => 'test.php',
 		'method' => 'GET'
 	]);
 	
 	echo $form->open();
-		echo (new Input)   ->setAttr('name', 'login');
+		echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '666');;
+		echo (new Input)->setAttr('name', 'login');
 		echo (new Password)->setAttr('name', 'passw');
 		echo new Submit;
 	echo $form->close();
