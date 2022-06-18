9f72b60176d4044d3c5697521004dd90f1bd75df
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:11:00 +0300

    62

--- Tag.php
+++ Tag.php
@@ -49,5 +49,13 @@
 				return '';
 			}
 		}
+		
+		public function setAttrs($attrs)
+		{
+			foreach ($attrs as $name => $value) {
+				$this->setAttr($name, $value);
+			}	
+			return $this;
+		}
 	}
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 	require_once 'Tag.php';
 
-	$tag = new Tag('div');
-	$tag->setAttr('id', 'test'); // откроем тег
-	$tag->removeAttr('id'); // удалим тег
-	echo $tag->Open();
-	echo 'sadas';
-	echo $tag->Close(); // закроем тег
+	$tag = new Tag('input');
 	
-	// выводит <div>sadas</div>
+	echo $tag
+		->setAttrs(['id' => 'test', 'class' => 'eee'])
+		->setAttr('type', 'text')
+		->open(); 
 		
+		// выведет <input id="test" class="eee" type="text">
 	
 ?>
