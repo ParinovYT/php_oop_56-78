6021330a0e28792ab5070ea757a349e5670072a3
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:37:58 +0300

    66

--- Tag.php
+++ Tag.php
@@ -98,6 +98,11 @@
 			}
 			
 			return $this;
-		}		
+		}
+		
+		public function getName() { return $this->name; }
+		public function getText() { return $this->getAttrsStr($this->attrs); }
+		public function getAttrs() { return var_dump($this->attrs); }
+		public function getAttr($attr) { return $this->attrs[$attr]; }
 	}
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 	require_once 'Tag.php';
 
-	echo (new Tag('input'))
-		->setAttr('class', 'eee zzz kkk') // добавим 3 класса
-		->removeClass('zzz') // удалим класс 'zzz'
-		->addClass('test')
-		->open(); // выведет <input class="eee kkk">
+	$tag = new Tag('input');
+
+	$tag->setAttr('class', 'eee zzz kkk'); // добавим 3 класса
+	$tag->removeClass('zzz'); // удалим класс 'zzz'
+	$tag->addClass('test');
+	echo nl2br($tag->open()."\n\n getName: "); // выведет <input class="eee kkk">
+	echo nl2br($tag->getName()."\n getAttrs: ");
+	echo nl2br($tag->getAttrs()."\n getAttr: ");
+	echo nl2br($tag->getAttr('class')."\n getText: ");
+	echo nl2br($tag->getText());
+	
 	
 ?>
