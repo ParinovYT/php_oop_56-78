03a23730673729fefe3beb2d207c4dfe17872040
Danila <gamer.xokks@yandex.ru>
2022-06-18 13:05:35 +0300

    69

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 	require_once 'iTag.php';
 	require_once 'Tag.php';
-	require_once 'Image.php';
-	$img = (new Image())->setAttrs(['src' => 'cat-005.png', 'width' =>'300', 'height'=>'200']);
-	echo $img->open();
+	require_once 'Link.php';
+	require_once 'menu.php';
+
+	print(nl2br($link1."\n"));	
+	print(nl2br($link2."\n"));	
+	print(nl2br($link3."\n"));	
+	print(nl2br($link4."\n"));	
+	print(nl2br($link5."\n"));	
+	print(nl2br($link6."(show)\n"));	
+	
+	echo (new Link)
+		->setAttr('href', 'menu/5.php')
+		->setAttr('class', 'link1 link2') // добавляем ссылке классы
+		->setText('index')
+		->show();
+	
+	
 ?>
