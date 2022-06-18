6f8dab498baa9fad8e0f4ee4c69594bad8dba798
Danila <gamer.xokks@yandex.ru>
2022-06-18 11:31:15 +0300

    59_1

--- index.php
+++ index.php
@@ -1,58 +1,7 @@
 <?php
-	function FileSizeConvert($bytes)
-	{
-		$bytes = floatval($bytes);
-			$arBytes = array(
-				0 => array(
-					"UNIT" => "TB",
-					"VALUE" => pow(1024, 4)
-				),
-				1 => array(
-					"UNIT" => "GB",
-					"VALUE" => pow(1024, 3)
-				),
-				2 => array(
-					"UNIT" => "MB",
-					"VALUE" => pow(1024, 2)
-				),
-				3 => array(
-					"UNIT" => "KB",
-					"VALUE" => 1024
-				),
-				4 => array(
-					"UNIT" => "B",
-					"VALUE" => 1
-				),
-			);
-
-		foreach($arBytes as $arItem)
-		{
-			if($bytes >= $arItem["VALUE"])
-			{
-				$result = $bytes / $arItem["VALUE"];
-				$result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
-				break;
-			}
-		}
-		return $result;
-	}
-
-
-	header('Content-Type: text/html; charset=utf-8');
-	require_once 'IFile.php';
-	require_once 'File.php';
+	require_once 'Tag.php';
+	$tag = new Tag('h3');
 	
-	$file = new File('ntoskrnl.exe');
-	print(nl2br("FilePath: ".$file->getPath()."\n"));
-	print(nl2br("FileDir: ".$file->getDir()."\n"));
-	print(nl2br("FileName: ".$file->getName()."\n"));
-	print(nl2br("FileExt: ".$file->getExt()."\n"));
-	print(nl2br("FileSize: ".FileSizeConvert($file->getSize())."\n"));
-	print(nl2br("FileText: не вывожу, файл binary\n"));
-	
-	#print(nl2br("\n\nFileText: \n\n".$file->getText()."\n"));
-	# не вывожу, файл binary.
-	
-	
+	echo $tag->Open().'text'.$tag->Close(); // выведет <div>text</div>
 	
 ?>
