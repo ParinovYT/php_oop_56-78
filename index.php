d5f3f6037e53f83ae9385cda59652b3542b6e9ff
Danila <gamer.xokks@yandex.ru>
2022-06-18 10:52:00 +0300

    57

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8');
 	require_once "date.php";
+	require_once "Interval.php";
 	
-	$date = new Date('2025-12-31');	
-	echo $date->getYear().nl2br("\n");  
-	echo $date->getMonth('ru').nl2br("\n"); 
-	echo $date->getMonth('en').nl2br("\n"); 
-	echo $date->getMonth().nl2br("\n"); 
-	echo $date->getMonth('haha').nl2br("\n"); 
-	echo $date->getDay().nl2br("\n\n");
-
-	echo $date->getWeekDay().nl2br("\n");   
-	echo $date->getWeekDay('ru').nl2br("\n"); 
-	echo $date->getWeekDay('en').nl2br("\n"); 
-	echo $date->getWeekDay('haha').nl2br("\n\n"); 
-
-	echo (new Date('2025-12-31'))->addYear(1).nl2br("\n");
-	echo (new Date('2025-12-31'))->addDay(1).nl2br("\n");  
-	echo (new Date('2025-12-31'))->subDay(3).nl2br("\n"); 
-	echo (new Date('2025-12-31'))->addYear(1).nl2br("\n"); 
+	$date1 = new Date('2025-12-31');
+	$date2 = new Date('2026-11-28');
+	
+	$interval = new Interval($date1, $date2);
+	
+	echo $interval->toDays();   // выведет разницу в днях
+	echo nl2br(" days\n");
+	echo $interval->toMonths();   // выведет разницу в месяцах
+	echo nl2br(" months\n");
+	echo $interval->toYears();   // выведет разницу в годах
+	echo nl2br(" years\n");
+	
+	
 ?>
