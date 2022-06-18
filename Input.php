cff0c95de02fdca97503cd87ddcfc656669796dd
Danila <gamer.xokks@yandex.ru>
2022-06-18 13:40:06 +0300

    72_3-4

--- Input.php
+++ Input.php
@@ -5,9 +5,25 @@
 		{
 			parent::__construct('input');
 		}
+		
+		public function open()
+		{
+			$inputName = $this->getAttr('name');
+			
+			// Если атрибут name задан у инпута:
+			if ($inputName) {
+				if (isset($_REQUEST[$inputName])) {
+					$value = $_REQUEST[$inputName];
+					$this->setAttr('value', $value);
+				}
+			}
+			
+			return parent::open();
+		}
+		
 		public function __toString()
 		{
-			return parent::open();
+			return $this->open();
 		}
 	}
 ?>
--- index.php
+++ index.php
@@ -4,12 +4,20 @@
 	require_once 'Form.php';
 	require_once 'Input.php';
 	
-	echo nl2br("\n\nПример без ореn\n\n");
-	
 	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	
 	echo $form->open();
-	echo (new Input)->setAttr('name', 'year');
+	echo (new Input)->setAttr('name', 'n1');
+	echo (new Input)->setAttr('name', 'n2');
+	echo (new Input)->setAttr('name', 'n3');
+	echo (new Input)->setAttr('name', 'n4');
+	echo (new Input)->setAttr('name', 'n5');
+	
 	echo (new Input)->setAttr('type', 'submit');
 	echo $form->close();
+	
+	if(isset($_GET['n1']) || isset($_GET['n2'])|| isset($_GET['n3'])|| isset($_GET['n4'])|| isset($_GET['n5'])){
+		echo $_GET['n1'] + $_GET['n2']+ $_GET['n3']+ $_GET['n4']+ $_GET['n5'];
+	}
+	
 ?>
