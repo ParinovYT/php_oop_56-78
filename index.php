a1abaf7f499fe3463fc402799c73045ee5274944
Danila <gamer.xokks@yandex.ru>
2022-06-18 13:29:10 +0300

    72_1

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 	require_once 'iTag.php';
 	require_once 'Tag.php';
 	require_once 'Form.php';
+	require_once 'Input.php';
 
-	$form = (new Form)->setAttrs([
-		'action' => 'test.php',
-		'method' => 'POST'
-	]);
+	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	
 	echo $form->open();
-		// здесь потом будут элементы формы
+	echo (new Input)->setAttr('name', 'year')->open();
+	echo (new Input)->setAttr('type', 'submit')->open();
 	echo $form->close();
-	
-	//выводит <form action="test.php" method="POST"></form>
+
 ?>
