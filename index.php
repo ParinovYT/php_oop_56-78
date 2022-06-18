129bb67172c05adf27865e01d0ccf8044c2294d4
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:21:03 +0300

    65

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 	require_once 'Tag.php';
 
-	$tag = new Tag('input');
-	
-	echo (new Tag('input'))->setAttr('name', 'name1')->open();
-	echo (new Tag('input'))->setAttr('name', 'name2')->open();
-	
-	//вывод <input name="name1"><input name="name2">
+	echo (new Tag('input'))
+		->setAttr('class', 'eee zzz kkk') // добавим 3 класса
+		->removeClass('zzz') // удалим класс 'zzz'
+		->addClass('test')
+		->open(); // выведет <input class="eee kkk">
 	
 ?>
