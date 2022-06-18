4dd6b65d11cbeba4cad360620d66dc468397f2f5
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:15:59 +0300

    64

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 
 	$tag = new Tag('input');
 	
-	echo $tag
-		->setAttr('id', 'test')
-		->setAttr('disabled', true)
-		->open();
-		
-		// выведет <input id="test" disabled>
+	echo (new Tag('input'))->setAttr('name', 'name1')->open();
+	echo (new Tag('input'))->setAttr('name', 'name2')->open();
+	
+	//вывод <input name="name1"><input name="name2">
 	
 ?>
