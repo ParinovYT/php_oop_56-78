a962ad71ce694151b1d6beceafc9148c2e04a1d5
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:00:02 +0300

    60

--- Tag.php
+++ Tag.php
@@ -2,45 +2,38 @@
 	class Tag
 	{
 		private $name;
+		private $attrs;
 		
-		public function __construct($name)
+		public function __construct($name, $attrs = [])
 		{
 			$this->name = $name;
+			$this->attrs = $attrs;
 		}
 		
-		public function Open($params = null)
+		public function Open()
 		{
-			if($this->name == "img")
-			{
-				return "<$this->name src='$params'";	
-			}
-			
-			if($params == "header")
-			{
-				return "<header><$this->name>";
-			}
-			
-			if(empty($params))
-			{
-				return "<$this->name>";	
+			$attrsStr = $this->getAttrsStr($this->attrs); 
+			return "<$this->name$attrsStr>";
+		}
+		
+		public function Close()
+		{
+			return "</$this->name>";
+		}
+		
+		private function getAttrsStr($attrs)
+		{
+			if (!empty($attrs)) {
+				$result = '';
+				
+				foreach ($attrs as $name => $value) {
+					$result .= " $name=\"$value\"";
+				}
+				
+				return $result;
+			} else {
+				return '';
 			}
 		}
-		public function Close($params = null)
-		{
-			if($this->name == "img")
-			{
-				return ">";	
-			}
-			
-			if($params == "header")
-			{
-				return "</$this->name></header>";
-			}
-			
-			if(empty($params))
-			{
-				return "</$this->name>";	
-			}
-		}		
 	}
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 	require_once 'Tag.php';
 	
-	$tag0 = new Tag('div');
-	$tag1 = new Tag('h2');
-	$tag2 = new Tag('img');
-	
-	echo $tag0->Open().'text'.$tag0->Close(); // выведет <div>text</div>
-	echo $tag1->Open("header").'text'.$tag1->Close("header"); // выведет <header><h2>text</h2></header>
-	echo $tag2->Open("cat-005.png").$tag2->Close(); // выведет <img src="cat-005.png">
+	$tag = new Tag('input', ['id' => 'test', 'class' => 'eee bbb']);
+	echo $tag->Open(); // выведет <input id="test" class="eee bbb">
 	
 ?>
