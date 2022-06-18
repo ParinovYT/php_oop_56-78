20e7309bb51c68ba94eddfce1bd35607e8f5f8ef
Danila <gamer.xokks@yandex.ru>
2022-06-18 11:52:14 +0300

    59_1-3

--- Tag.php
+++ Tag.php
@@ -8,13 +8,39 @@
 			$this->name = $name;
 		}
 		
-		public function Open()
+		public function Open($params = null)
 		{
-			return "<$this->name>";
+			if($this->name == "img")
+			{
+				return "<$this->name src='$params'";	
+			}
+			
+			if($params == "header")
+			{
+				return "<header><$this->name>";
+			}
+			
+			if(empty($params))
+			{
+				return "<$this->name>";	
+			}
 		}
-		public function Close()
+		public function Close($params = null)
 		{
-			return "</$this->name>";
+			if($this->name == "img")
+			{
+				return ">";	
+			}
+			
+			if($params == "header")
+			{
+				return "</$this->name></header>";
+			}
+			
+			if(empty($params))
+			{
+				return "</$this->name>";	
+			}
 		}		
 	}
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 	require_once 'Tag.php';
-	$tag = new Tag('h3');
 	
-	echo $tag->Open().'text'.$tag->Close(); // выведет <div>text</div>
+	$tag0 = new Tag('div');
+	$tag1 = new Tag('h2');
+	$tag2 = new Tag('img');
+	
+	echo $tag0->Open().'text'.$tag0->Close(); // выведет <div>text</div>
+	echo $tag1->Open("header").'text'.$tag1->Close("header"); // выведет <header><h2>text</h2></header>
+	echo $tag2->Open("cat-005.png").$tag2->Close(); // выведет <img src="cat-005.png">
 	
 ?>
