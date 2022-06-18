3556de87670cf482a620db21311536ba72e61cd3
Danila <gamer.xokks@yandex.ru>
2022-06-18 12:09:18 +0300

    61

--- Tag.php
+++ Tag.php
@@ -4,10 +4,9 @@
 		private $name;
 		private $attrs;
 		
-		public function __construct($name, $attrs = [])
+		public function __construct($name)
 		{
 			$this->name = $name;
-			$this->attrs = $attrs;
 		}
 		
 		public function Open()
@@ -19,6 +18,21 @@
 		public function Close()
 		{
 			return "</$this->name>";
+		}
+		
+		public function setAttr($name, $value)
+		{
+			$this->attrs[$name] = $value;
+			return $this; 
+		}
+		
+		public function removeAttr($name)
+		{
+			if(array_key_exists($name, $this->attrs))
+			{
+				unset($this->attrs[$name]);
+			}
+			return $this;
 		}
 		
 		private function getAttrsStr($attrs)
--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 	require_once 'Tag.php';
+
+	$tag = new Tag('div');
+	$tag->setAttr('id', 'test'); // откроем тег
+	$tag->removeAttr('id'); // удалим тег
+	echo $tag->Open();
+	echo 'sadas';
+	echo $tag->Close(); // закроем тег
 	
-	$tag = new Tag('input', ['id' => 'test', 'class' => 'eee bbb']);
-	echo $tag->Open(); // выведет <input id="test" class="eee bbb">
+	// выводит <div>sadas</div>
+		
 	
 ?>
