a1c978f933f1a04a5e3a713a2a448e8687362860
Danila <gamer.xokks@yandex.ru>
2022-06-18 13:32:23 +0300

    72_2

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 	require_once 'Tag.php';
 	require_once 'Form.php';
 	require_once 'Input.php';
-
+	
+	echo nl2br("\n\nПример без ореn\n\n");
+	
 	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	
 	echo $form->open();
-	echo (new Input)->setAttr('name', 'year')->open();
-	echo (new Input)->setAttr('type', 'submit')->open();
+	echo (new Input)->setAttr('name', 'year');
+	echo (new Input)->setAttr('type', 'submit');
 	echo $form->close();
-
 ?>
